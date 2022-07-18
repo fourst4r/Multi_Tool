6ec150d6c17f5e2f353253756dfb9eca96e3ff8b
Tobias <Tobia@DESKTOP-K02VCHB>
2022-07-18 09:10:04 +0200

    Fail safe on fetching pr2 build version

--- UserInterface/Menu/Options/Settings/Options/UserSettingsOptions/Options/AddUserOptions/Options/AddUserOption.cs
+++ UserInterface/Menu/Options/Settings/Options/UserSettingsOptions/Options/AddUserOptions/Options/AddUserOption.cs
@@ -30,30 +30,84 @@ namespace UserInterface.Menu.Options.Settings.Options.UserSettingsOptions.Option
                 WriteLine(Environment.NewLine + "\tError, invalid input!", ErrorColor);
         }
 
-        private void GetRequiredInfo(bool usePassword) {
+        private void GetRequiredInfo(bool usePassword)
+        {
             IsInputValid = true;
             _username = GetUsername(false);
 
             if (IsInputValid)
             {
-                if(usePassword)
+                if (usePassword)
                     _token = GetTokenFromPassword();
                 else
                     _token = GetToken(false);
             }
         }
 
+        private string GetPr2BuildVersionManually()
+        {
+            try
+            {
+                // This is an ugly trick
+                // I have a map that I need to manually update with the Build Version number
+
+                int levelId = Constants.InfoLevelId;
+                var level  = _builder.DownloadLevel(levelId);
+                var version = level?.TextArt1?.FirstOrDefault()?.Text;
+
+                if (string.IsNullOrWhiteSpace(version))
+                    return string.Empty;
+
+                var text = System.Web.HttpUtility.UrlDecode(version).Trim();
+                var result = ReplaceAsciiValues(text);
+
+                return result;
+
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private string ReplaceAsciiValues(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return text;
+
+            var culture = CultureInfo.InvariantCulture;
+
+            for (int i = 32; i < 256; i++)
+            {
+                if (i == 35)
+                    continue;
+
+                var value = "#" + i.ToString(culture);
+                var symbol = ((char)i).ToString(culture);
+
+                text = text.Replace(value, symbol);
+            }
+
+            return text.Replace("#35", "#");
+        }
+
         private string GetPr2BuildVersion()
         {
-            WriteLine("\tChecking PR2 version..." + Environment.NewLine);
+            WriteLine("\tChecking PR2 version...");
 
             var info = _accessor.GetPr2Version();
 
             if (string.IsNullOrWhiteSpace(info?.BuildVersion))
             {
+                var version = GetPr2BuildVersionManually();
+
+                if(!string.IsNullOrWhiteSpace(version)) 
+                    return version;
+
                 WriteLine("\tFailed to read the newest PR2 build version.", ErrorColor);
                 showInvalidInputMessage = false;
                 IsInputValid = false;
+
                 return string.Empty;
             }
 
