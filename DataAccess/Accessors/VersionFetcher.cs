1d66b6a17137f6a35bd0d704300edf0591afc857
Tobias <Tobia@DESKTOP-K02VCHB>
2022-07-24 14:35:14 +0200

    Get BuildVersion

--- DataAccess/Accessors/VersionFetcher.cs
+++ DataAccess/Accessors/VersionFetcher.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using DataAccess.DataStructures;
+﻿using DataAccess.DataStructures;
 using Newtonsoft.Json.Linq;
 
 namespace DataAccess.Accessors
@@ -28,7 +25,33 @@ namespace DataAccess.Accessors
             VersionInfo.Url          = json?.GetValue("url")?.Value<string>() ?? string.Empty;
             VersionInfo.Time         = json?.GetValue("time")?.Value<string>() ?? string.Empty;
             VersionInfo.Version      = json?.GetValue("version")?.Value<string>() ?? string.Empty;
-            VersionInfo.BuildVersion = json?.GetValue("build_version")?.Value<string>() ?? string.Empty;
+            VersionInfo.BuildVersion = GetBuildVersion(json);
+        }
+
+        private string GetBuildVersion(JObject json)
+        {
+            // the name of the build version inside the JSON file is unknown
+            // as it has not been implemented yet
+            // so i am just dummy trying some values and hope it will work once Bls199 update the API
+            // so I dont have to make a new release of the tool later
+            
+            if(json == null)
+                return string.Empty;
+
+            var names = new string[] { "build_version", "buildversion", "buildVersion", "build", "BuildVersion", 
+                                       "client_version", "clientversion", "clientVersion", "client", "ClientVersion",
+                                       "code_version", "codeversion", "codeVersion", "CodeVersion"};
+
+            foreach(var name in names)
+            {
+                var value = json?.GetValue(name)?.Value<string>() ?? string.Empty;
+
+                if (!string.IsNullOrWhiteSpace(value))
+                    return value;
+            }
+           
+            return string.Empty;
+
         }
     }
 }
