4f5a7e9569e80f26cd273934120e4b6a5d20a5a0
Tobias <Tobia@DESKTOP-K02VCHB>
2022-07-29 20:29:24 +0200

    asdf

--- DataAccess/Accessors/SearchLevels.cs
+++ DataAccess/Accessors/SearchLevels.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Net;
-using System.Text;
+﻿using System.Globalization;
 
 namespace DataAccess.Accessors
 {
